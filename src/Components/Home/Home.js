1e3cab51a5b48bec82ca222f63c847168e64a313
Yuki (//w//) <indraniko20@gmail.com>
2022-11-21 17:15:34 +0700

    add email to hire button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,44 +1,50 @@
 import React from 'react'
 import "./Home.css";
 import Typewriter from 'typewriter-effect';
-import { MdDarkMode,MdLightMode} from "react-icons/md";
+import {MdDarkMode, MdLightMode} from "react-icons/md";
 import Fade from 'react-reveal/Fade';
 
-const Home = ({theme,changeTheme}) => {
-  return (
-    <div className="container-fluid home" id="home">
-        <div className="theme-changer" onClick={changeTheme}>
-            {theme === "light" ? (<MdDarkMode size={40}/>) : (<p className="sun-theme-icon"><MdLightMode size={40}/></p>)}
+const Home = ({theme, changeTheme}) => {
+    return (
+        <div className="container-fluid home" id="home">
+            <div className="theme-changer" onClick={changeTheme}>
+                {theme === "light" ? (<MdDarkMode size={40}/>) : (
+                    <p className="sun-theme-icon"><MdLightMode size={40}/></p>)}
+            </div>
+            <div className="container home-content">
+                <Fade left>
+                    <h1>Hi I'm Indra Niko Saputra</h1>
+                    <h3>
+                        <Typewriter
+                            options={{
+                                strings: ['Diponegoro University Student', 'Informatics Bachelor', 'To Be Fullstack Developer'],
+                                autoStart: true,
+                                loop: true,
+                                delay: 75
+                            }}
+                        />
+                    </h3>
+                </Fade>
+                <Fade bottom>
+                    <div className="button-action">
+                            <div className="hire-me-button no-apperance">
+                                <a href="mailto:mailto:indraniko20@gmail.com"
+                                   target="_blank" rel="noreferrer">
+                                    Hire Me
+                                </a>
+                            </div>
+
+                        <div className="get-resume-button">
+                            <a href="https://drive.google.com/drive/u/0/folders/1yqkU59923D9AKk1ctAJthidr1kWFSGAI"
+                               target="_blank" rel="noreferrer">
+                                Get Resume
+                            </a>
+                        </div>
+                    </div>
+                </Fade>
+            </div>
         </div>
-      <div className="container home-content">
-          <Fade left>
-              <h1>Hi I'm Indra Niko Saputra</h1>
-              <h3>
-                  <Typewriter
-                      options={{
-                          strings: ['Diponegoro University Student', 'Informatics Bachelor','To Be Fullstack Developer'],
-                          autoStart: true,
-                          loop: true,
-                          delay: 75
-                      }}
-                  />
-              </h3>
-          </Fade>
-          <Fade bottom>
-              <div className="button-action">
-                  <div className="hire-me-button">
-                      Hire Me
-                  </div>
-                  <div className="get-resume-button">
-                      <a href="https://drive.google.com/drive/u/0/folders/1yqkU59923D9AKk1ctAJthidr1kWFSGAI" target="_blank" rel="noreferrer">
-                          Get Resume
-                      </a>
-                  </div>
-              </div>
-          </Fade>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Home
